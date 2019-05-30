167f95dda483a93909d42f13f12831e94c8daac3
Firas.AbdElnour <firas2439@gmail.com>
2019-05-30 18:57:08 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+import App from './validation2/App2';
+
+ReactDOM.render(<App />, document.querySelector('#container'));
